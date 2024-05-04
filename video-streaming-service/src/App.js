ac81b5caf5020d54d8ba6228344f06b549df53c8
Project1 <yuvraajbhama@gmail.com>
2024-05-04 23:56:46 +0530

    initial commit

---
+++ video-streaming-service/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to our Video Streaming Site!</h1>
+      <video width="700px" height="400px" controls>
+        <source src="https://d1qn0dne19h33l.cloudfront.net/SMS_Spam_Classifier.ipynb%20-%20Colaboratory%20-%20Google%20Chrome%202024-02-28%2007-21-50.mp4" type="video/mp4"></source>
+      </video>
+    </div>
+  );
+}
+
+export default App;
